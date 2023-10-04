c5626c2372ee263f15bbce96b0f662542cc9dcfc
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-10-04 23:47:33 +0200

    ended hw-1_nodejs

---
+++ README.md
@@ -0,0 +1,4 @@
+https://ibb.co/c2NLqST
+https://ibb.co/GQxnzBh
+https://ibb.co/BT9XkX4
+https://ibb.co/bB8QWfD
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('welcome');
+import yargs from "yargs";
+
+import * as contactsService from "./contacts.js";
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const contactsList = await contactsService.listContacts();
+      return console.table(contactsList);
+
+    case 'get':
+      const getContact = await contactsService.getContactById(id);
+      return console.log(getContact);
+
+    case 'add':
+      const newContact = await contactsService.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case 'remove':
+        const delContact = await contactsService.removeContact(id);
+        return console.log(delContact);
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+// invokeAction({action: 'list'})
+// invokeAction({action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw"})
+// invokeAction({action: "add", name: "Chaim Lewis", email: "dui.in@egetlacus.ca", phone: "(294) 840-6685"})
+
+const {argv} = yargs(process.argv.slice(2));
+invokeAction(argv);
